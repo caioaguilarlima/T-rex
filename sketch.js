43c40ee4fa784e39e0da40714fdda58e586cf472
caioaguilarlima <104800489+caioaguilarlima@users.noreply.github.com>
2022-05-09 20:06:25 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 
 var trex, trex_running;
-
+var edges;
 function preload(){
   trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
 
@@ -8,7 +8,7 @@ function preload(){
 
 function setup(){
   createCanvas(600,200);
-  
+  edges = createEdgeSprites()
   //crie um sprite de trex
   trex = createSprite(50, 160, 20, 50);
   trex.addAnimation("running", trex_running);
@@ -21,6 +21,7 @@ function draw(){
   if (keyDown("space")) {
     trex.velocityY = -10;
   }
-  
+  trex.velocityY = trex.velocityY +0.5;
+trex.collide(edges[3])
   drawSprites();
 }
