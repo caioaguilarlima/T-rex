2737c96c8bd212b16cab73ae66053c2d451625d1
andressalessa <lessadasilveiraandressa@gmail.com>
2022-05-23 19:43:21 -0300

    Create Clouds

--- sketch.js
+++ sketch.js
@@ -8,17 +8,18 @@
 
 
 var trex, trex_running;
-var ground, groundImg;
-var edges;
+var ground, groundImg, invisibleGround;
+var cloud, cloudImg;
+
 
 function preload(){
   trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
   groundImg = loadImage("ground2.png");
+  cloudImg = loadImage("cloud.png");
 }
 
 function setup(){
   createCanvas(600,200);
-  edges = createEdgeSprites();
 
   //crie um sprite de trex
   trex = createSprite(50, 160, 20, 50);
@@ -28,11 +29,15 @@ function setup(){
   ground = createSprite(200, 180, 400, 10);
   ground.addImage("ground", groundImg);
   ground.x = ground.width/2;
+
+  invisibleGround = createSprite(200, 190, 400, 10);
+  invisibleGround.visible = false;
 }
 
 function draw(){
   background("black");
 
+  // adicionar condição corrigir pulo duplicado
   if (keyDown("space")) {
     trex.velocityY = -10;
   }
@@ -41,10 +46,17 @@ function draw(){
   }
   ground.velocityX = -4; 
   trex.velocityY = trex.velocityY +0.5;
-  trex.collide(edges[3]);
+  trex.collide(invisibleGround);
   
-  
+  createClouds();
   
   
   drawSprites();
+}
+
+function createClouds()
+{
+  cloud = createSprite(600, 100, 40, 10);
+  cloud.velocityX = -3;
+
 }
